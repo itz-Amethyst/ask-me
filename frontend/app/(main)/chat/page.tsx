96127e4b3cd61c2496c2e6913450698ca7052aad
itz-amethyst <pransermi@gmail.com>
2024-04-13 13:27:52 +0330

    feat: chat

---
+++ frontend/app/(main)/chat/page.tsx
@@ -0,0 +1,109 @@
+"use client";
+
+import axios from "axios";
+
+import { useState } from "react";
+
+import { Loader } from "@/components/loader";
+import Image from 'next/image'
+
+
+const ChatPage = () => {
+	const [isLoading, setIsLoading] = useState(false);
+	const [message, setMessage] = useState('');
+	const [messages, setMessages] = useState([]);
+	const [showMessage, setShowMessage] = useState(false);
+
+	// main
+	const handleSubmit = () => {
+		setShowMessage(true);
+		setIsLoading(true)
+
+		// Simulate loading for 1 second
+		setTimeout(() => {
+			const userMessage = { content: message, sender: 'user' };
+
+			// Later, you would replace this with the response from the bot retrieved via Axios
+			const botResponse = 'This is from bot';
+
+			// Create a new message object for the bot's response
+			const botMessage = { content: botResponse, sender: 'bot' };
+
+			setMessages([...messages, userMessage, botMessage]);
+			setIsLoading(false)
+
+			// Clear the input field
+			setMessage('');
+		}, 1000);
+	};
+
+
+	return (
+		<>
+			{isLoading && (
+				<div className="p-8 rounded-lg w-full flex items-center justify-center bg-muted">
+					<Loader />
+				</div>
+			)}
+			<div className="flex flex-col flex-grow overflow-auto">
+				<div className="flex px-4 py-3">
+					<div className="h-10 w-10 rounded flex-shrink-0 bg-gray-300">
+						<div className="w-10 h-10 relative animate-pulse">
+							<Image src="/loading_one_icon_155179.png" alt="Sam" className="h-6 w-6 rounded-full" fill />
+						</div>
+					</div>
+					<div className="ml-2">
+						<div className="-mt-1 flex items-center">
+							<span className="text-sm font-semibold">BOT</span>
+
+						</div>
+						<p className="text-sm">Anyone know if Frodo is awake yet?</p>
+						<div className="flex space-x-2 mt-1">
+							<button className="flex items-center pl-1 pr-2 h-5 bg-gray-300 hover:bg-gray-400 rounded-full text-xs">
+								<span>🔥</span>
+								<span className="ml-1 font-medium">2</span>
+							</button>
+						</div>
+					</div>
+				</div>
+
+
+				<div className="flex flex-col items-center mt-2">
+
+				</div>
+
+
+				{showMessage && messages.map((msg, index) => (
+					<div key={index} className="flex px-4 py-3">
+						<div className="h-10 w-10 rounded flex-shrink-0 bg-gray-300"></div>
+						<div className="ml-2">
+							<div className="-mt-1">
+								<span className="text-sm font-semibold">{msg.sender === 'user' ? 'User' : 'Bot'}</span>
+								<span className="ml-1 text-xs text-gray-500">01:26</span>
+							</div>
+							<p className="text-sm">{msg.content}</p>
+								
+						</div>
+					</div>
+				))}
+
+			</div>
+
+
+			<div className="h-12 bg-white px-5 pb-4 text-x">
+				<div className="flex items-center border-2 border-gray-300 rounded-sm p-1">
+					<input disabled={isLoading} value={message} onChange={(e) => setMessage(e.target.value)} className="flex-grow text-sm px-3 border-l border-gray-300 ml-1" style={{ resize: 'none' }} placeholder="Message council-of-elrond" cols="" rows="1"></input>
+
+
+					<button disabled={isLoading} className="flex-shrink flex items-center justify-center h-6 w-6 rounded hover:bg-gray-200" onClick={handleSubmit}>
+						<svg className="h-4 w-4 transform rotate-90" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+							<path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z" />
+						</svg>
+					</button>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default ChatPage;
