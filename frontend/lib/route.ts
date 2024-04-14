906bfc498c401c462e37d1316017fba053e78eb0
itz-amethyst <pransermi@gmail.com>
2024-04-14 23:39:30 +0330

    :recycle: updated

--- frontend/lib/route.ts
+++ frontend/lib/route.ts
@@ -2,4 +2,4 @@
 const DEBUG = process.env.DEBUG === 'true';
 
 // Define the base URL based on the DEBUG value
-export const BASE_URL = DEBUG ? 'https://ask-me-dwd5uweh6-callmemilads-projects.vercel.app' : 'http://127.0.0.1:8000';
+export const BASE_URL = DEBUG ? 'https://ask-me-api.vercel.app' : 'http://127.0.0.1:8000';
